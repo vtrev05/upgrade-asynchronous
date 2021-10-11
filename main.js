428c28063ce400156d2e133633da9f7d29826416
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-11 12:57:18 +0200

    done

--- main.js
+++ main.js
@@ -13,27 +13,35 @@
 agify() */
 
 //2.2
-const baseUrl = 'https://api.nationalize.io';
-
+/* const baseUrl = 'https://api.nationalize.io';
+const input = document.querySelector('input')
 const btn = document.querySelector('.button')
 
 const nationalize = async () => {
-    const result = await fetch(baseUrl)
+    const result = await fetch(`${baseUrl}?name=${input.value}`)
     const json = await result.json()
     console.log(json)
 }
 
-btn.addEventListener('click', nationalize)
+btn.addEventListener('click', nationalize) */
 
 //2.3
 const btn2 = document.querySelector('.button2')
 const input = document.querySelector('input')
 
 const agify = async () => {
-    const result = await fetch(`https://api.agify.io?name=${input.value}`)
+    const result = await fetch(`https://api.nationalize.io?name=${input.value}`)
     const json = await result.json()
-    document.querySelector('.title').innerHTML = `<p class=paragraph">El nombre ${json.name} se repite ${json.count} veces y tiene ${json.age} años</p> `
+    const countries = json.country.map((element)=>(
+        {
+        id: element.country_id,
+        prob: element.probability
+        }
+    ))
+    console.log(countries)
+    document.querySelector('.title').innerHTML = `<p class=paragraph">El nombre ${json.name} tiene un ${countries[0].prob} de ser de ${countries[0].id} <p class=paragraph">El nombre ${json.name} tiene un ${countries[1].prob} de ser de ${countries[1].id} <p class=paragraph">El nombre ${json.name} tiene un ${countries[2].prob} de ser de ${countries[2].id}`
 }
+
 btn2.addEventListener('click', agify)
 
 //2.4
